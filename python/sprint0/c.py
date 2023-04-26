db14464d20c1dfe7098f062600e62bf03339a4eb
Александр Ячменев <yachmenyov.alex@yandex.ru>
2023-04-27 02:29:14 +0700

    Issues upgrade moving_average.

--- python/sprint0/c.py
+++ python/sprint0/c.py
@@ -1,16 +1,15 @@
 from typing import List, Tuple
 
 
-def moving_average(arr: List[int], window_size: int) -> List[float]:
-    # Здесь реализация вашего решения
-    result = []
-    for begin_index in range(0, len(arr) - window_size + 1):
-        end_index = begin_index + window_size
-        # Просматриваем окно шириной window_size.
-        current_sum = 0
-        for v in arr[begin_index:end_index]:
-            current_sum += v
-        current_avg = current_sum / window_size
+def moving_average(timeseries, K):
+    result = []  # Пустой массив.
+    # Первый раз вычисляем значение честно и сохраняем результат.
+    current_sum = sum(timeseries[0:K])
+    result.append(current_sum / K)
+    for i in range(0, len(timeseries) - K):
+        current_sum -= timeseries[i]
+        current_sum += timeseries[i+K]
+        current_avg = current_sum / K
         result.append(current_avg)
     return result
 
