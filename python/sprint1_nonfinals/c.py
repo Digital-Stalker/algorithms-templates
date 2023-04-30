b5067c34903eb413ef79a297c4f8ce72911fe453
Александр Ячменев <yachmenyov.alex@yandex.ru>
2023-04-30 19:02:52 +0700

    Issues get_neighbours.

--- python/sprint1_nonfinals/c.py
+++ python/sprint1_nonfinals/c.py
@@ -1,8 +1,29 @@
 from typing import List, Tuple
 
+
 def get_neighbours(matrix: List[List[int]], row: int, col: int) -> List[int]:
     # Здесь реализация вашего решения
-    pass
+    top_idx = (row - 1, col)
+    right_idx = (row, col + 1)
+    bot_idx = (row + 1, col)
+    left_idx = (row, col - 1)
+
+    idx_list = [
+        top_idx,
+        right_idx,
+        bot_idx,
+        left_idx
+    ]
+
+    res = []
+    for idx in idx_list:
+        row_idx = idx[0]
+        col_idx = idx[1]
+        if row_idx >= 0 and col_idx >= 0:
+            neighbour = matrix[row_idx][col_idx]
+            res.append(neighbour)
+    return sorted(res)
+
 
 def read_input() -> Tuple[List[List[int]], int, int]:
     n = int(input())
@@ -13,6 +34,7 @@ def read_input() -> Tuple[List[List[int]], int, int]:
     row = int(input())
     col = int(input())
     return matrix, row, col
+
 
 matrix, row, col = read_input()
 print(" ".join(map(str, get_neighbours(matrix, row, col))))
