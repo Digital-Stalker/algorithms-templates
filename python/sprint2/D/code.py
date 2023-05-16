e69597f7152d0014be42d0eff571881aa3d4abb5
Александр Ячменев <yachmenyov.alex@yandex.ru>
2023-05-16 16:46:47 +0700

    Issues D solution.

--- python/sprint2/D/code.py
+++ python/sprint2/D/code.py
@@ -1,18 +1,28 @@
 # ! change LOCAL to False before submitting !
 # set LOCAL to True for local testing
 
-LOCAL = True
+LOCAL = False
 
 if LOCAL:
-    class Node:  
-        def __init__(self, value, next_item=None):  
-            self.value = value  
+    class Node:
+        def __init__(self, value, next_item=None):
+            self.value = value
             self.next_item = next_item
+
 
 def solution(node, elem):
     # Your code
     # ヽ(´▽`)/
-    pass
+    index = 0
+    while elem:
+        if node is None:
+            return -1
+        if elem == node.value:
+            return index
+        else:
+            node = node.next_item
+            index += 1
+
 
 def test():
     node3 = Node("node3", None)
@@ -21,6 +31,7 @@ def test():
     node0 = Node("node0", node1)
     idx = solution(node0, "node2")
     assert idx == 2
+
 
 if __name__ == '__main__':
     test()
